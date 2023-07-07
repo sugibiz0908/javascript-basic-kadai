46a4eef129488b73ba7eeae2f02b9d00849d0e7b
Sugibiz <132025719+sugibiz0908@users.noreply.github.com>
2023-07-08 02:15:07 +0900

    Add files via upload

---
+++ kadai_019/event.js
@@ -0,0 +1,6 @@
+const btnBtn = document.getElementById('btn');
+
+btnBtn.addEventListener('click', () => {
+  const text = document.getElementById('text');
+  text.textContent = 'ボタンをクリックしました';
+});
